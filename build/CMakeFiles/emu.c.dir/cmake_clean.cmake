77311704783fddaa305ff235613956c03c1154b4
alex <peter.alex.morris@gmail.com>
2025-06-24 10:50:09 +0900

    looking for help lol

---
+++ build/CMakeFiles/emu.c.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/emu.c.dir/chip8.c.o"
+  "CMakeFiles/emu.c.dir/chip8.c.o.d"
+  "emu.c"
+  "emu.c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/emu.c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/emulator.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/emulator.dir/chip8.c.o"
+  "CMakeFiles/emulator.dir/chip8.c.o.d"
+  "CMakeFiles/emulator.dir/display.c.o"
+  "CMakeFiles/emulator.dir/display.c.o.d"
+  "CMakeFiles/emulator.dir/emu.c.o"
+  "CMakeFiles/emulator.dir/emu.c.o.d"
+  "bin/emulator"
+  "bin/emulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/emulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
